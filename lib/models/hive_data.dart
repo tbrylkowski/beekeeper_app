e29432fa9fadacbce4f1c5ae381aa0fa0e3485ee
tadeusz.brylkowski-p <tadeusz.brylkowski@globallogic.com>
2019-08-27 15:14:55 +0200

    Added hive model and checking DatePicker

---
+++ lib/models/hive_data.dart
@@ -0,0 +1,9 @@
+
+class Hive {
+  String _startDate;
+  int _hiveNumber;
+  Hive(this._startDate, this._hiveNumber);
+
+  String get startDate => _startDate;
+  int get hiveNumber => _hiveNumber;
+}
--- lib/screens/add_hive_screen.dart
+++ lib/screens/add_hive_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddNewHiveScreen extends StatefulWidget {
   static String name = "AddNewHiveScreen";
+
   @override
   _AddNewHiveScreenState createState() => _AddNewHiveScreenState();
 }
 
 class _AddNewHiveScreenState extends State<AddNewHiveScreen> {
+  String _startButtonText = "Wybierz date startu";
+  String _startTime;
+
+  pickStartDateTime() async {
+    DateTime dateTime = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2018),
+        lastDate: DateTime(2050));
+        _startTime = DateFormat("dd.MM.yyyy").format(dateTime);
+        setState(() {
+          _startButtonText = _startTime;
+        });
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +30,15 @@ class _AddNewHiveScreenState extends State<AddNewHiveScreen> {
         title: Text("Dodaj nowy Ul"),
       ),
       body: Container(
-        child: Center(child: Text("Hello")),
+        child: Center(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                    onPressed: pickStartDateTime,
+                    child: Text(_startButtonText)),
+                FlatButton(onPressed: null, child: Text("Ul number")),
+              ],
+            )),
       ),
     );
   }
